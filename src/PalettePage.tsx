d88cfd7ea66bfdef7b06103c7a71d3b91f9ba96e
longsangstan <longsangstan@gmail.com>
2019-08-28 23:54:32 +0800

    add illustration select

--- src/PalettePage.tsx
+++ src/PalettePage.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+
+// @ts-ignore
+import Select from "react-select"; // a bug for value prop type for grouped options?
 
 import Pattern from "./illustrations/Pattern";
 import Gradients from "./illustrations/Gradients";
@@ -7,17 +10,64 @@ interface PalettePageProps {
   colors: tinycolor.Instance[];
 }
 
+const uiOptions = [
+  { value: "bootstrap", label: "Bootstrap" },
+  { value: "material", label: "Material Design" }
+];
+
+const graphicsOptions = [
+  { value: "pattern", label: "Pattern" },
+  { value: "gradient", label: "Gradient" }
+];
+
+const groupedOptions = [
+  {
+    label: "UI",
+    options: uiOptions
+  },
+  {
+    label: "Graphics",
+    options: graphicsOptions
+  }
+];
+
 const PalettePage: React.FC<PalettePageProps> = ({ colors }) => {
+  const [selectedOption, setSelectedOption] = useState(graphicsOptions[0]);
+
+  const renderIllustration = () => {
+    if (selectedOption.value === "pattern") {
+      return (
+        <div style={{ height: 350, width: 350 }}>
+          <Pattern colors={colors} />
+        </div>
+      );
+    }
+
+    if (selectedOption.value === "gradient") {
+      return (
+        <div style={{ margin: 10 }}>
+          <Gradients colors={colors} />
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="palette-page">
-      <h1>Illustrations</h1>
-      <div style={{ height: 350, width: 350 }}>
-        <Pattern colors={colors} />
+      <div style={{ width: 200, marginTop: 25, marginBottom: 25 }}>
+        <Select
+          className="illustrations-dropdown"
+          classNamePrefix="illustrations-dropdown"
+          value={selectedOption}
+          onChange={(value: { value: string; label: string }) => {
+            setSelectedOption(value);
+          }}
+          options={groupedOptions}
+          isSearchable={false}
+        />
       </div>
 
-      <div style={{ margin: 10 }}>
-        <Gradients colors={colors} />
-      </div>
+      {renderIllustration()}
     </div>
   );
 };
