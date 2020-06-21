03ce75a22c6b5b4f1f9d4022f4566b6b2c0ae7b1
longsangstan <longsangstan@gmail.com>
2020-06-21 21:41:39 +0100

    add amazon links

---
+++ src/constants/amazon-links.ts
@@ -0,0 +1,48 @@
+type Link = {
+  title: string;
+  url: string;
+  img?: string;
+};
+
+export const searchResults: readonly Link[] = [
+  {
+    title: "Design books",
+    url:
+      "https://www.amazon.co.uk/s?k=Design&i=stripbooks&camp=1634&creative=6738&linkCode=ur2&linkId=ac12a645bf63727ea2d311b7a8a77cae&tag=longsangstan-21",
+  },
+];
+
+export const designBooks: readonly Link[] = [
+  {
+    title: "The Design of Everyday Things",
+    url: "https://amzn.to/3hOEpYr",
+    img:
+      "https://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=GB&ASIN=B06XCCZJ4L&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=longsangstan-21",
+  },
+  {
+    title:
+      "Book of Ideas - a journal of creative direction and graphic design - volume 1",
+    url: "https://amzn.to/3hQq1PB",
+    img:
+      "https://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=GB&ASIN=0993540007&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=longsangstan-21",
+  },
+  {
+    title: "The Secret Lives of Colour: RADIO 4's BOOK OF THE WEEK",
+    url: "https://amzn.to/3enfJnJ",
+    img:
+      "https://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=GB&ASIN=1473630819&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=longsangstan-21",
+  },
+  {
+    title:
+      "Don't Make Me Think, Revisited: A Common Sense Approach to Web Usability",
+    url: "https://amzn.to/2Ykuty2",
+    img:
+      "https://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=GB&ASIN=0321965515&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=longsangstan-21",
+  },
+  {
+    title: "100 Things Every Designer Needs to Know About People",
+    url: "https://amzn.to/3en39Fb",
+    img:
+      "https://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=GB&ASIN=0321767535&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=longsangstan-21",
+  },
+];
